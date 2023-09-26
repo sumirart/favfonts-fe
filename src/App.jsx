3f1f30667b33aacb06e6e2809370f751a794b08c
pratama.sumirat <pratama.sumirat@shopee.com>
2023-09-27 00:37:42 +0700

    fix: typo import footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import CardContainer from "./components/CardContainer";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 function App() {
   const [searchFont, setSearchFont] = useState("");
   const [sampleText, setSampleText] = useState("");
