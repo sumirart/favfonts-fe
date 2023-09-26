2f2a2828e91f4b2405b35b106b1fa500aa580b39
pratama.sumirat <pratama.sumirat@shopee.com>
2023-09-27 00:36:01 +0700

    fix: typo on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import CardContainer from "./components/CardContainer";
 import Footer from "./components/footer";
 function App() {
